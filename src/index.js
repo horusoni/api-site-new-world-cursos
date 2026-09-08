b76a52172ff499e58166e19d3a70a0b069073e00
Gilberto Costa <127106886+horusoni@users.noreply.github.com>
2026-09-08 11:59:11 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://www.newworldcursos.com.br", // depois você pode restringir
+    origin: "*", // depois você pode restringir
 }));
 
 // 🔥 multer em memória (OBRIGATÓRIO na Vercel)
